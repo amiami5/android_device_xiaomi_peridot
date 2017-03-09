e74e450643fb181b1a244a4d44cb31a5db025d3b
Etan Cohen <etancohen@google.com>
2017-03-08 16:43:38 -0800

    peridot: Declare WiFi Aware support to WiFi HAL
     * The Wi-Fi Aware feature added in Android 8.0 enables
       supporting devices to discover, connect, and range
       (added in Android 9) to one another directly using
       the Wi-Fi Aware protocol without internet or cellular
       network access.
    
     * This feature, built upon the Wi-Fi Alliance (WFA) Wi-Fi
       Aware specification (versions 2.0 and 3.0), allows easy
       sharing of high-throughput data among trusted devices
       and apps that are otherwise off-network.
    
       https://source.android.com/devices/tech/connect/wifi-aware
    
    Change-Id: I18ff20e7466b8e8fd598381b730fb7db391e9ad4
    Signed-off-by: Chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -461,7 +461,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/peridot/peridot-vendor.mk)
