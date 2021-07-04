447f272cd9a057851130bd104565d5675483eaa2
kenway214 <kenway214@outlook.com>
2021-07-04 12:53:33 +0000

    peridot: parts: Introduced TouchSamplingService
      for reapplying HTSR settings periodically, reducing redundant operations for improved efficiency.
    
    - Added BootCompletedReceiver to ensure HTSR state restoration during device boot.
    - Imported SEpolicy for device parts.
    - Enhanced service logic by incorporating BroadcastReceiver for:
     • ACTION_USER_PRESENT (device unlock).
     • ACTION_SCREEN_ON (screen wake).
     • Dynamically reapplies the touch sampling rate when triggered.
    
    Co-Authored-By: raghavt20 <raghavt20@aospa.co>
    Change-Id: I0b9a4d5ba788b8bf177206eaf12b01ee7df03364
    Signed-off-by: ramshell688 <ramonzost68@gmail.com>

--- parts/src/com/xiaomi/settings/utils/FileUtils.java
+++ parts/src/com/xiaomi/settings/utils/FileUtils.java
@@ -10,6 +10,7 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -54,27 +55,113 @@ public final class FileUtils {
         return 0;
     }
 
-    public static void writeLine(String fileName, String value) {
-        BufferedWriter writerValue = null;
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
         try {
-            writerValue = new BufferedWriter(new FileWriter(fileName));
-            writerValue.write(value);
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
         } catch (FileNotFoundException e) {
-            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
         } catch (IOException e) {
-            Log.e(TAG, "Could not write to file " + fileName, e);
+            Log.e(TAG, "Could not read from file " + fileName, e);
         } finally {
             try {
-                if (writerValue != null) {
-                    writerValue.close();
+                if (reader != null) {
+                    reader.close();
                 }
             } catch (IOException e) {
                 // Ignored, not much we can do anyway
             }
         }
+        return line;
+    }
+
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+        return true;
     }
 
     public static void writeLine(String fileName, int value) {
         writeLine(fileName, Integer.toString(value));
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
     }
 }
