daa8af9140d80049a8bc7280518438c5063b4275
ami <github@amisuke.com>
2024-11-06 01:43:56 +0900

    commitIDの変更
    Change-Id: I51741ccd6ce762ffb5010c093ccc78909e227dfb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,16 @@
+echo "miuicamera"
 cat vendor/xiaomi/peridot/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk.part* > vendor/xiaomi/peridot/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
+
 echo "指紋認証の修正"
-cd frameworks/base && git fetch https://github.com/amiami5/android_frameworks_base && git cherry-pick e38018129a7f4f4b18ca85953522e0c2e7f9f3a4 && \
+cd frameworks/base && git fetch https://github.com/amiami5/android_frameworks_base && git cherry-pick d1e39761893400346be1de3dbd27b3dfbf795c6e
+
+echo "指紋認証の修正その他その１"
+git cherry-pick 07c04537ead76c9bb6b8f8bf2a7cea4eaac15eb5
+
+echo "指紋認証の修正その他その２"
+git cherry-pick 114e8d18925664dfa63601ad977061d57455e639
+
+echo "指紋認証の修正その他その３"
+git cherry-pick 02ca6daee92f269eea8b8002804119ad623d3e4a
+
 cd ../../
-#cat vendor/xiaomi/peridot/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk.part* > vendor/xiaomi/peridot/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
