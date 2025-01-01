9980d65aaae17d2d318e8e51fd718121893bb966
ami <github@amisuke.com>
2025-01-01 23:28:25 +0900

    commitIDの変更
    Change-Id: If0ad76f72c1164738ef8b446e005de659abfa95a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 echo "指紋認証の修正"
-cd frameworks/base && git fetch https://github.com/amiami5/android_frameworks_base 15.0 && git cherry-pick 729f41657b406aef35eaaffd13eac57f367de6a6
+cd frameworks/base && git fetch https://github.com/amiami5/android_frameworks_base 15.0 && git cherry-pick 2196c754b8f8e93050f12b60b4f3d8e7b8fa3520
 cd ../../
 
 echo "updaterを自分に向ける"
