58da9ce180058782116ecf9de4e8d4401677024c
ami <github@amisuke.com>
2025-03-01 18:19:16 +0900

    Revert "peridot: Drop Lineage Health HAL"
    充電コントロールはほしい
    This reverts commit b3ce75c13c956e6f2a3bd81220eee067dffb0caa.
    
    Change-Id: If1795370f4008c5350288be2bb02301879c15247

--- device.mk
+++ device.mk
@@ -320,6 +320,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker.config.json
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Logging
 PRODUCT_PACKAGES += \
     MatLog
