49ab6d6c9aad01383477026833eeca0c47a1fdf1
ramshell688 <ramonzost68@gmail.com>
2025-03-17 23:57:46 +0700

    peridot: Update from OS2.0.100.0.VNPMIXM

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -24,10 +24,12 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 24069PC21G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="peridot_global-user 14 UKQ1.240116.001 V816.0.9.0.UNPMIXM release-keys" \
-    BuildFingerprint=POCO/peridot_global/peridot:14/UKQ1.240116.001/V816.0.10.0.UNPMIXM:user/release-keys \
-    DeviceName=peridot \
-    DeviceProduct=peridot_global
+    BuildDesc="peridot_global-user 15 AQ3A.240912.001 OS2.0.100.0.VNPMIXM release-keys" \
+    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.100.0.VNPMIXM:user/release-keys \
+    DeviceName=peridot
+    DeviceProduct=peridot_global \
+    SystemName=peridot_global \
+    SystemDevice=peridot
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
