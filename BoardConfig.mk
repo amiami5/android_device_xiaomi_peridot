f5a4f363bb2090ff6cdabdc1642c9addb5ae80f6
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-18 18:05:43 +0800

    peridot: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,6 @@ PRODUCT_COPY_FILES += \
 
 # Init
 $(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):libinit_peridot)
-TARGET_RECOVERY_DEVICE_MODULES := libinit_peridot
 
 # Kernel modules
 DLKM_MODULES_PATH := $(PREBUILT_PATH)/modules/vendor_dlkm
