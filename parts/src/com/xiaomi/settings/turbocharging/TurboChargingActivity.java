project-dynamic/device_xiaomi_peridot

peridot: parts: Implement turbo charging V1
inspired by..

https://github.com/yaap/packages_apps_FastCharge

GuidixX/device_xiaomi_peridot@9973272

yaap/device_motorola_cancunf@168c11d

Signed-off-by: kenway214 <kenway214@outlook.com>

parts/src/org/lineageos/settings/turbocharging/TurboChargingActivity.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.turbocharging;
+ 
+ import android.os.Bundle;
+ import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+ 
+ import org.lineageos.settings.R;
+ 
+ public class TurboChargingActivity extends CollapsingToolbarBaseActivity {
+ 
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.turbocharging_layout);
+         getFragmentManager()
+                 .beginTransaction()
+                 .replace(R.id.content_frame, new TurboChargingFragment())
+                 .commit();
+     }
+ }

parts/src/org/lineageos/settings/turbocharging/TurboChargingBootReceiver.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.turbocharging;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class TurboChargingBootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "TurboChargingBootReceiver";
+    private static final String PREF_TURBO_ENABLED = "turbo_enable";
+    private static final String PREF_TURBO_CURRENT = "turbo_current";
+    private static final String PREF_SPORTS_MODE = "sports_mode";
+
+    private static final String PROP_TURBO_CURRENT = "persist.sys.turbo_charge_current";
+    private static final String DEFAULT_OFF_VALUE = "6000000";
+    private static final String DEFAULT_ON_VALUE = "9750000";
+
+    private static final String SPORTS_MODE_NODE = "/sys/class/qcom-battery/sport_mode";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean turboEnabled = prefs.getBoolean(PREF_TURBO_ENABLED, false);
+        String turboValue = turboEnabled ? prefs.getString(PREF_TURBO_CURRENT, DEFAULT_ON_VALUE)
+                : DEFAULT_OFF_VALUE;
+        boolean sportsEnabled = prefs.getBoolean(PREF_SPORTS_MODE, false);
+
+        try {
+            Class<?> sp = Class.forName("android.os.SystemProperties");
+            Method setProp = sp.getMethod("set", String.class, String.class);
+            setProp.invoke(null, PROP_TURBO_CURRENT, turboValue);
+            Log.i(TAG, "Boot: Set " + PROP_TURBO_CURRENT + " to " + turboValue);
+        } catch (Exception e) {
+            Log.e(TAG, "Boot: Failed to set property " + PROP_TURBO_CURRENT, e);
+        }
+
+        String sportsValue = sportsEnabled ? "1" : "0";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(SPORTS_MODE_NODE))) {
+            writer.write(sportsValue);
+            Log.i(TAG, "Boot: Sports Mode node set to " + sportsValue);
+        } catch (IOException e) {
+            Log.e(TAG, "Boot: Failed to update Sports Mode node", e);
+        }
+    }
+}

parts/src/org/lineageos/settings/turbocharging/TurboChargingService.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.turbocharging;
+ 
+ import android.app.Service;
+ import android.content.Intent;
+ import android.os.Handler;
+ import android.os.IBinder;
+ import android.os.UEventObserver;
+ import android.util.Log;
+ 
+ import androidx.preference.PreferenceManager;
+ 
+ import org.lineageos.settings.R;
+ 
+ import java.io.BufferedReader;
+ import java.io.BufferedWriter;
+ import java.io.FileReader;
+ import java.io.FileWriter;
+ import java.io.IOException;
+ import java.lang.reflect.Method;
+ 
+ public class TurboChargingService extends Service {
+     private static final String TAG = "TurboChargingService";
+     private static final String CHARGE_CURRENT_FILE = "/sys/class/power_supply/battery/constant_charge_current";
+     private static final String USB_ONLINE_FILE = "/sys/class/power_supply/usb/online";
+ 
+     private static final String PROP_TURBO_CURRENT = "persist.sys.turbo_charge_current";
+ 
+     private static final String DEFAULT_OFF_VALUE = "6000000";
+     private static final String DEFAULT_ON_VALUE = "9750000";
+ 
+     private UEventObserver mObserver;
+     private Handler mHandler = new Handler();
+     private Runnable mMonitorRunnable;
+ 
+     @Override
+     public void onCreate() {
+         Log.d(TAG, "Starting TurboChargingService");
+ 
+         mObserver = new UEventObserver() {
+             @Override
+             public void onUEvent(UEvent event) {
+                 String chargerStatus = event.get("POWER_SUPPLY_ONLINE");
+                 if (chargerStatus != null && chargerStatus.equals("1")) {
+                     updateChargeCurrent();
+                 }
+             }
+         };
+         mObserver.startObserving("DEVPATH=/sys/class/power_supply/usb");
+ 
+         updateChargeCurrent();
+         startMonitoring();
+     }
+ 
+     private void updateChargeCurrent() {
+         boolean turboEnabled = PreferenceManager.getDefaultSharedPreferences(this)
+                 .getBoolean("turbo_enable", false);
+         String desiredValue = turboEnabled ?
+                 PreferenceManager.getDefaultSharedPreferences(this)
+                         .getString("turbo_current", DEFAULT_ON_VALUE)
+                 : DEFAULT_OFF_VALUE;
+         Log.i(TAG, "Updating charge current with value: " + desiredValue);
+         setChargingProperty(desiredValue);
+     }
+ 
+     private void setChargingProperty(String value) {
+         try {
+             Class<?> sp = Class.forName("android.os.SystemProperties");
+             Method setProp = sp.getMethod("set", String.class, String.class);
+             setProp.invoke(null, PROP_TURBO_CURRENT, value);
+             Log.i(TAG, "Property " + PROP_TURBO_CURRENT + " set to " + value);
+             writeChargeCurrent(value);
+         } catch (Exception e) {
+             Log.e(TAG, "Failed to set property " + PROP_TURBO_CURRENT, e);
+         }
+     }
+ 
+     private void writeChargeCurrent(String value) {
+         try (BufferedWriter writer = new BufferedWriter(new FileWriter(CHARGE_CURRENT_FILE))) {
+             writer.write(value);
+             Log.i(TAG, "Updated charging current node to " + value);
+         } catch (IOException e) {
+             Log.e(TAG, "Failed to update charge current", e);
+         }
+     }
+ 
+     private String readChargeCurrent() {
+         try (BufferedReader reader = new BufferedReader(new FileReader(CHARGE_CURRENT_FILE))) {
+             return reader.readLine();
+         } catch (IOException e) {
+             Log.e(TAG, "Failed to read charge current", e);
+             return "";
+         }
+     }
+ 
+     private void startMonitoring() {
+         mMonitorRunnable = new Runnable() {
+             @Override
+             public void run() {
+                 try {
+                     String currentNodeValue = readChargeCurrent();
+                     String desiredValue = getSystemProperty(PROP_TURBO_CURRENT, DEFAULT_OFF_VALUE);
+                     if (!desiredValue.equals(currentNodeValue)) {
+                         Log.i(TAG, "Detected mismatch in sysfs node (found: " + currentNodeValue +
+                                 ", desired: " + desiredValue + "). Updating...");
+                         writeChargeCurrent(desiredValue);
+                     }
+                 } finally {
+                     mHandler.postDelayed(this, 5000);
+                 }
+             }
+         };
+         mHandler.post(mMonitorRunnable);
+     }
+ 
+     private String getSystemProperty(String key, String def) {
+         try {
+             Class<?> sp = Class.forName("android.os.SystemProperties");
+             Method getProp = sp.getMethod("get", String.class, String.class);
+             return (String) getProp.invoke(null, key, def);
+         } catch (Exception e) {
+             Log.e(TAG, "Failed to get system property " + key, e);
+             return def;
+         }
+     }
+ 
+     @Override
+     public int onStartCommand(Intent intent, int flags, int startId) {
+         return START_STICKY;
+     }
+ 
+     @Override
+     public void onDestroy() {
+         mObserver.stopObserving();
+         mHandler.removeCallbacks(mMonitorRunnable);
+         super.onDestroy();
+     }
+ 
+     @Override
+     public IBinder onBind(Intent intent) {
+         return null;
+     }
+ }

parts/src/org/lineageos/settings/turbocharging/TurboChargingTile.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.turbocharging;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.widget.Toast;
+import androidx.preference.PreferenceManager;
+import org.lineageos.settings.R;
+
+public class TurboChargingTile extends TileService {
+
+    private static final String PREF_TURBO_ENABLED = "turbo_enable";
+
+    @Override
+    public void onClick() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean turboEnabled = prefs.getBoolean(PREF_TURBO_ENABLED, false);
+        boolean newState = !turboEnabled;
+        prefs.edit().putBoolean(PREF_TURBO_ENABLED, newState).apply();
+        TurboChargingUtil.applyTurboSetting(this);
+        Toast.makeText(this, newState ? getString(R.string.toast_turbo_on) : getString(R.string.toast_turbo_off),
+                Toast.LENGTH_SHORT).show();
+        Tile tile = getQsTile();
+        if (tile != null) {
+            tile.setState(newState ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+            tile.updateTile();
+        }
+    }
+}

parts/src/org/lineageos/settings/turbocharging/TurboChargingUtil.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.turbocharging;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import java.lang.reflect.Method;
+
+public class TurboChargingUtil {
+
+    private static final String PREF_TURBO_ENABLED = "turbo_enable";
+    private static final String PREF_TURBO_CURRENT = "turbo_current";
+    private static final String PROP_TURBO_CURRENT = "persist.sys.turbo_charge_current";
+    private static final String DEFAULT_OFF_VALUE = "6000000";
+    private static final String DEFAULT_ON_VALUE = "9750000";
+
+    public static void applyTurboSetting(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean turboEnabled = prefs.getBoolean(PREF_TURBO_ENABLED, false);
+        String turboValue = turboEnabled ? prefs.getString(PREF_TURBO_CURRENT, DEFAULT_ON_VALUE)
+                : DEFAULT_OFF_VALUE;
+
+        try {
+            Class<?> sp = Class.forName("android.os.SystemProperties");
+            Method setProp = sp.getMethod("set", String.class, String.class);
+            setProp.invoke(null, PROP_TURBO_CURRENT, turboValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

amiami5/android_device_xiaomi_peridot

parts/src/com/xiaomi/settings/turbocharging/TurboChargingActivity.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.turbocharging;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+
+import com.xiaomi.settings.R;
+
+public class TurboChargingActivity extends CollapsingToolbarBaseActivity {
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.turbocharging_layout);
+         getFragmentManager()
+                 .beginTransaction()
+                 .replace(R.id.content_frame, new TurboChargingFragment())
+                 .commit();
+     }
+ }

parts/src/com/xiaomi/settings/turbocharging/TurboChargingBootReceiver.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.turbocharging;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class TurboChargingBootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "TurboChargingBootReceiver";
+    private static final String PREF_TURBO_ENABLED = "turbo_enable";
+    private static final String PREF_TURBO_CURRENT = "turbo_current";
+    private static final String PREF_SPORTS_MODE = "sports_mode";
+
+    private static final String PROP_TURBO_CURRENT = "persist.sys.turbo_charge_current";
+    private static final String DEFAULT_OFF_VALUE = "6000000";
+    private static final String DEFAULT_ON_VALUE = "9750000";
+
+    private static final String SPORTS_MODE_NODE = "/sys/class/qcom-battery/sport_mode";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean turboEnabled = prefs.getBoolean(PREF_TURBO_ENABLED, false);
+        String turboValue = turboEnabled ? prefs.getString(PREF_TURBO_CURRENT, DEFAULT_ON_VALUE)
+                : DEFAULT_OFF_VALUE;
+        boolean sportsEnabled = prefs.getBoolean(PREF_SPORTS_MODE, false);
+
+        try {
+            Class<?> sp = Class.forName("android.os.SystemProperties");
+            Method setProp = sp.getMethod("set", String.class, String.class);
+            setProp.invoke(null, PROP_TURBO_CURRENT, turboValue);
+            Log.i(TAG, "Boot: Set " + PROP_TURBO_CURRENT + " to " + turboValue);
+        } catch (Exception e) {
+            Log.e(TAG, "Boot: Failed to set property " + PROP_TURBO_CURRENT, e);
+        }
+
+        String sportsValue = sportsEnabled ? "1" : "0";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(SPORTS_MODE_NODE))) {
+            writer.write(sportsValue);
+            Log.i(TAG, "Boot: Sports Mode node set to " + sportsValue);
+        } catch (IOException e) {
+            Log.e(TAG, "Boot: Failed to update Sports Mode node", e);
+        }
+    }
+}

parts/src/com/xiaomi/settings/turbocharging/TurboChargingService.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.turbocharging;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.UEventObserver;
+import android.util.Log;
+
+import androidx.preference.PreferenceManager;
+
+import com.xiaomi.settings.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class TurboChargingService extends Service {
+    private static final String TAG = "TurboChargingService";
+    private static final String CHARGE_CURRENT_FILE = "/sys/class/power_supply/battery/constant_charge_current";
+    private static final String USB_ONLINE_FILE = "/sys/class/power_supply/usb/online";
+
+    private static final String PROP_TURBO_CURRENT = "persist.sys.turbo_charge_current";
+
+    private static final String DEFAULT_OFF_VALUE = "6000000";
+    private static final String DEFAULT_ON_VALUE = "9750000";
+
+    private UEventObserver mObserver;
+    private Handler mHandler = new Handler();
+    private Runnable mMonitorRunnable;
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "Starting TurboChargingService");
+
+        mObserver = new UEventObserver() {
+            @Override
+            public void onUEvent(UEvent event) {
+                String chargerStatus = event.get("POWER_SUPPLY_ONLINE");
+                if (chargerStatus != null && chargerStatus.equals("1")) {
+                    updateChargeCurrent();
+                }
+            }
+        };
+        mObserver.startObserving("DEVPATH=/sys/class/power_supply/usb");
+
+        updateChargeCurrent();
+        startMonitoring();
+    }
+
+    private void updateChargeCurrent() {
+        boolean turboEnabled = PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean("turbo_enable", false);
+        String desiredValue = turboEnabled ?
+                PreferenceManager.getDefaultSharedPreferences(this)
+                         .getString("turbo_current", DEFAULT_ON_VALUE)
+                : DEFAULT_OFF_VALUE;
+        Log.i(TAG, "Updating charge current with value: " + desiredValue);
+        setChargingProperty(desiredValue);
+    }
+
+    private void setChargingProperty(String value) {
+        try {
+            Class<?> sp = Class.forName("android.os.SystemProperties");
+            Method setProp = sp.getMethod("set", String.class, String.class);
+            setProp.invoke(null, PROP_TURBO_CURRENT, value);
+            Log.i(TAG, "Property " + PROP_TURBO_CURRENT + " set to " + value);
+            writeChargeCurrent(value);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to set property " + PROP_TURBO_CURRENT, e);
+        }
+    }
+
+    private void writeChargeCurrent(String value) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CHARGE_CURRENT_FILE))) {
+            writer.write(value);
+            Log.i(TAG, "Updated charging current node to " + value);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to update charge current", e);
+        }
+    }
+
+    private String readChargeCurrent() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(CHARGE_CURRENT_FILE))) {
+            return reader.readLine();
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read charge current", e);
+            return "";
+        }
+    }
+
+    private void startMonitoring() {
+        mMonitorRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String currentNodeValue = readChargeCurrent();
+                    String desiredValue = getSystemProperty(PROP_TURBO_CURRENT, DEFAULT_OFF_VALUE);
+                    if (!desiredValue.equals(currentNodeValue)) {
+                        Log.i(TAG, "Detected mismatch in sysfs node (found: " + currentNodeValue +
+                                ", desired: " + desiredValue + "). Updating...");
+                        writeChargeCurrent(desiredValue);
+                    }
+                } finally {
+                    mHandler.postDelayed(this, 5000);
+                }
+            }
+        };
+        mHandler.post(mMonitorRunnable);
+    }
+
+    private String getSystemProperty(String key, String def) {
+        try {
+            Class<?> sp = Class.forName("android.os.SystemProperties");
+            Method getProp = sp.getMethod("get", String.class, String.class);
+            return (String) getProp.invoke(null, key, def);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to get system property " + key, e);
+            return def;
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        mObserver.stopObserving();
+        mHandler.removeCallbacks(mMonitorRunnable);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

parts/src/com/xiaomi/settings/turbocharging/TurboChargingTile.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.turbocharging;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.UserHandle;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import android.widget.Toast;
+import androidx.preference.PreferenceManager;
+import com.xiaomi.settings.R;
+
+public class TurboChargingTile extends TileService {
+
+    private static final String PREF_TURBO_ENABLED = "turbo_enable";
+
+    @Override
+    public void onClick() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean turboEnabled = prefs.getBoolean(PREF_TURBO_ENABLED, false);
+        boolean newState = !turboEnabled;
+        prefs.edit().putBoolean(PREF_TURBO_ENABLED, newState).apply();
+        TurboChargingUtil.applyTurboSetting(this);
+        Toast.makeText(this, newState ? getString(R.string.toast_turbo_on) : getString(R.string.toast_turbo_off),
+                Toast.LENGTH_SHORT).show();
+        Tile tile = getQsTile();
+        if (tile != null) {
+            tile.setState(newState ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+            tile.updateTile();
+        }
+    }
+}

parts/src/com/xiaomi/settings/turbocharging/TurboChargingUtil.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.turbocharging;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
+import java.lang.reflect.Method;
+
+public class TurboChargingUtil {
+
+    private static final String PREF_TURBO_ENABLED = "turbo_enable";
+    private static final String PREF_TURBO_CURRENT = "turbo_current";
+    private static final String PROP_TURBO_CURRENT = "persist.sys.turbo_charge_current";
+    private static final String DEFAULT_OFF_VALUE = "6000000";
+    private static final String DEFAULT_ON_VALUE = "9750000";
+
+    public static void applyTurboSetting(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean turboEnabled = prefs.getBoolean(PREF_TURBO_ENABLED, false);
+        String turboValue = turboEnabled ? prefs.getString(PREF_TURBO_CURRENT, DEFAULT_ON_VALUE)
+                : DEFAULT_OFF_VALUE;
+
+        try {
+            Class<?> sp = Class.forName("android.os.SystemProperties");
+            Method setProp = sp.getMethod("set", String.class, String.class);
+            setProp.invoke(null, PROP_TURBO_CURRENT, turboValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}