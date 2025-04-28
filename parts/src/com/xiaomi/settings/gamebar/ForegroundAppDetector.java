Project-Flare-Devices/device_xiaomi_peridot

peridot: parts: Introduce GameBar v4.0
Signed-off-by: kenway214 <kenway214@outlook.com>

peridot: parts: Add FPS meter support with selectable method for GameBar

- Introduced `GameBarFpsMeter` class to support FPS overlay using the new Android 13+ TaskFpsCallback API.
- Added fallback legacy sysfs method (`/sys/class/drm/sde-crtc-0/measured_fps`) for older devices.
- Updated GameBar to show FPS using selected method.

Signed-off-by: kenway214 <kenway214@outlook.com>

bump to v4

parts/src/org/lineageos/settings/gamebar/ForegroundAppDetector.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.gamebar;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class ForegroundAppDetector {
+
+    private static final String TAG = "ForegroundAppDetector";
+
+    public static String getForegroundPackageName(Context context) {
+
+        String pkg = tryGetRunningTasks(context);
+        if (pkg != null) {
+            return pkg;
+        }
+        pkg = tryReflectActivityTaskManager();
+        if (pkg != null) {
+            return pkg;
+        }
+        return "Unknown";
+    }
+
+    private static String tryGetRunningTasks(Context context) {
+        try {
+            if (context.checkSelfPermission("android.permission.GET_TASKS")
+                == PackageManager.PERMISSION_GRANTED) {
+
+                ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+                List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+                if (tasks != null && !tasks.isEmpty()) {
+                    ActivityManager.RunningTaskInfo top = tasks.get(0);
+                    if (top.topActivity != null) {
+                        return top.topActivity.getPackageName();
+                    }
+                }
+            } else {
+                Log.w(TAG, "GET_TASKS permission not granted to this system app?");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "tryGetRunningTasks error: ", e);
+        }
+        return null;
+    }
+
+    private static String tryReflectActivityTaskManager() {
+        try {
+            Class<?> atmClass = Class.forName("android.app.ActivityTaskManager");
+            Method getServiceMethod = atmClass.getDeclaredMethod("getService");
+            getServiceMethod.setAccessible(true);
+            Object atmService = getServiceMethod.invoke(null);
+            Method getTasksMethod = atmService.getClass().getMethod("getTasks", int.class);
+            @SuppressWarnings("unchecked")
+            List<?> taskList = (List<?>) getTasksMethod.invoke(atmService, 1);
+            if (taskList != null && !taskList.isEmpty()) {
+
+                Object firstTask = taskList.get(0);
+
+                Class<?> rtiClass = firstTask.getClass();
+                Method getTopActivityMethod = rtiClass.getDeclaredMethod("getTopActivity");
+                Object compName = getTopActivityMethod.invoke(firstTask);
+                if (compName != null) {
+
+                    Method getPackageNameMethod = compName.getClass().getMethod("getPackageName");
+                    String pkgName = (String) getPackageNameMethod.invoke(compName);
+                    return pkgName;
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "tryReflectActivityTaskManager error: ", e);
+        }
+        return null;
+    }
+}

parts/src/org/lineageos/settings/gamebar/GameBarAppRemoverActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.gamebar;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import org.lineageos.settings.R;
+
+public class GameBarAppRemoverActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_bar_app_selector);
+        setTitle("Remove Auto-Enable Apps");
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new GameBarAppRemoverFragment())
+                .commit();
+        }
+    }
+}

parts/src/org/lineageos/settings/gamebar/GameBarAppSelectorActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.gamebar;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import org.lineageos.settings.R;
+
+public class GameBarAppSelectorActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_bar_app_selector);
+        setTitle("Select Apps for GameBar");
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new GameBarAppSelectorFragment())
+                .commit();
+        }
+    }
+}

parts/src/org/lineageos/settings/gamebar/GameBarAutoAppsAdapter.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.gamebar;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import org.lineageos.settings.R;
+import java.util.List;
+
+public class GameBarAutoAppsAdapter extends RecyclerView.Adapter<GameBarAutoAppsAdapter.ViewHolder> {
+
+    public interface OnAppRemoveListener {
+        void onAppRemove(ApplicationInfo appInfo);
+    }
+
+    private PackageManager packageManager;
+    private List<ApplicationInfo> apps;
+    private OnAppRemoveListener listener;
+
+    public GameBarAutoAppsAdapter(PackageManager packageManager, List<ApplicationInfo> apps, OnAppRemoveListener listener) {
+        this.packageManager = packageManager;
+        this.apps = apps;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public GameBarAutoAppsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_bar_app_selector_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameBarAutoAppsAdapter.ViewHolder holder, int position) {
+        final ApplicationInfo appInfo = apps.get(position);
+        holder.appName.setText(appInfo.loadLabel(packageManager));
+        holder.appPackage.setText(appInfo.packageName);
+        holder.appIcon.setImageDrawable(appInfo.loadIcon(packageManager));
+        holder.itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onAppRemove(appInfo);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView appName;
+        TextView appPackage;
+        ImageView appIcon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            appName = itemView.findViewById(R.id.app_name);
+            appPackage = itemView.findViewById(R.id.app_package);
+            appIcon = itemView.findViewById(R.id.app_icon);
+        }
+    }
+}

parts/src/org/lineageos/settings/gamebar/GameBarMemInfo.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.gamebar;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GameBarMemInfo {
+
+    public static String getRamUsage() {
+        long memTotal = 0;
+        long memAvailable = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("/proc/meminfo"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("MemTotal:")) {
+                    memTotal = parseMemValue(line);
+                } else if (line.startsWith("MemAvailable:")) {
+                    memAvailable = parseMemValue(line);
+                }
+                if (memTotal > 0 && memAvailable > 0) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            return "N/A";
+        }
+
+        if (memTotal == 0) {
+            return "N/A";
+        }
+
+        long usedKb = (memTotal - memAvailable);
+        long usedMb = usedKb / 1024;
+        return String.valueOf(usedMb);
+    }
+
+    private static long parseMemValue(String line) {
+        String[] parts = line.split("\\s+");
+        if (parts.length < 3) {
+            return 0;
+        }
+        try {
+            return Long.parseLong(parts[1]);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}

amiami5/android_device_xiaomi_peridot

parts/src/com/xiaomi/settings/gamebar/ForegroundAppDetector.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.gamebar;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class ForegroundAppDetector {
+
+    private static final String TAG = "ForegroundAppDetector";
+
+    public static String getForegroundPackageName(Context context) {
+
+        String pkg = tryGetRunningTasks(context);
+        if (pkg != null) {
+            return pkg;
+        }
+        pkg = tryReflectActivityTaskManager();
+        if (pkg != null) {
+            return pkg;
+        }
+        return "Unknown";
+    }
+
+    private static String tryGetRunningTasks(Context context) {
+        try {
+            if (context.checkSelfPermission("android.permission.GET_TASKS")
+                == PackageManager.PERMISSION_GRANTED) {
+
+                ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+                List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+                if (tasks != null && !tasks.isEmpty()) {
+                    ActivityManager.RunningTaskInfo top = tasks.get(0);
+                    if (top.topActivity != null) {
+                        return top.topActivity.getPackageName();
+                    }
+                }
+            } else {
+                Log.w(TAG, "GET_TASKS permission not granted to this system app?");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "tryGetRunningTasks error: ", e);
+        }
+        return null;
+    }
+
+    private static String tryReflectActivityTaskManager() {
+        try {
+            Class<?> atmClass = Class.forName("android.app.ActivityTaskManager");
+            Method getServiceMethod = atmClass.getDeclaredMethod("getService");
+            getServiceMethod.setAccessible(true);
+            Object atmService = getServiceMethod.invoke(null);
+            Method getTasksMethod = atmService.getClass().getMethod("getTasks", int.class);
+            @SuppressWarnings("unchecked")
+            List<?> taskList = (List<?>) getTasksMethod.invoke(atmService, 1);
+            if (taskList != null && !taskList.isEmpty()) {
+
+                Object firstTask = taskList.get(0);
+
+                Class<?> rtiClass = firstTask.getClass();
+                Method getTopActivityMethod = rtiClass.getDeclaredMethod("getTopActivity");
+                Object compName = getTopActivityMethod.invoke(firstTask);
+                if (compName != null) {
+
+                    Method getPackageNameMethod = compName.getClass().getMethod("getPackageName");
+                    String pkgName = (String) getPackageNameMethod.invoke(compName);
+                    return pkgName;
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "tryReflectActivityTaskManager error: ", e);
+        }
+        return null;
+    }
+}

parts/src/com/xiaomi/settings/gamebar/GameBarAppRemoverActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.gamebar;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.xiaomi.settings.R;
+
+public class GameBarAppRemoverActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_bar_app_selector);
+        setTitle("Remove Auto-Enable Apps");
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new GameBarAppRemoverFragment())
+                .commit();
+        }
+    }
+}

parts/src/com/xiaomi/settings/gamebar/GameBarAppSelectorActivity.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.gamebar;
+
+import android.os.Bundle;
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.xiaomi.settings.R;
+
+public class GameBarAppSelectorActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_bar_app_selector);
+        setTitle("Select Apps for GameBar");
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new GameBarAppSelectorFragment())
+                .commit();
+        }
+    }
+}

parts/src/com/xiaomi/settings/gamebar/GameBarAutoAppsAdapter.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.gamebar;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.xiaomi.settings.R;
+import java.util.List;
+
+public class GameBarAutoAppsAdapter extends RecyclerView.Adapter<GameBarAutoAppsAdapter.ViewHolder> {
+
+    public interface OnAppRemoveListener {
+        void onAppRemove(ApplicationInfo appInfo);
+    }
+
+    private PackageManager packageManager;
+    private List<ApplicationInfo> apps;
+    private OnAppRemoveListener listener;
+
+    public GameBarAutoAppsAdapter(PackageManager packageManager, List<ApplicationInfo> apps, OnAppRemoveListener listener) {
+        this.packageManager = packageManager;
+        this.apps = apps;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public GameBarAutoAppsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_bar_app_selector_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameBarAutoAppsAdapter.ViewHolder holder, int position) {
+        final ApplicationInfo appInfo = apps.get(position);
+        holder.appName.setText(appInfo.loadLabel(packageManager));
+        holder.appPackage.setText(appInfo.packageName);
+        holder.appIcon.setImageDrawable(appInfo.loadIcon(packageManager));
+        holder.itemView.setOnClickListener(v -> {
+            if (listener != null) {
+                listener.onAppRemove(appInfo);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView appName;
+        TextView appPackage;
+        ImageView appIcon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            appName = itemView.findViewById(R.id.app_name);
+            appPackage = itemView.findViewById(R.id.app_package);
+            appIcon = itemView.findViewById(R.id.app_icon);
+        }
+    }
+}

parts/src/com/xiaomi/settings/gamebar/GameBarMemInfo.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright (C) 2025 kenway214
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaomi.settings.gamebar;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class GameBarMemInfo {
+
+    public static String getRamUsage() {
+        long memTotal = 0;
+        long memAvailable = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("/proc/meminfo"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("MemTotal:")) {
+                    memTotal = parseMemValue(line);
+                } else if (line.startsWith("MemAvailable:")) {
+                    memAvailable = parseMemValue(line);
+                }
+                if (memTotal > 0 && memAvailable > 0) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            return "N/A";
+        }
+
+        if (memTotal == 0) {
+            return "N/A";
+        }
+
+        long usedKb = (memTotal - memAvailable);
+        long usedMb = usedKb / 1024;
+        return String.valueOf(usedMb);
+    }
+
+    private static long parseMemValue(String line) {
+        String[] parts = line.split("\\s+");
+        if (parts.length < 3) {
+            return 0;
+        }
+        try {
+            return Long.parseLong(parts[1]);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}