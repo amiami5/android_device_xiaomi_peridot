912afe5ed51f648cc5ecfb2a0130b3bac14bf144
Guidix <guidixm@gmail.com>
2025-06-23 18:01:04 +0300

    peridot: Use gpt utils from source
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,7 +178,6 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 
 # Recovery
-$(call soong_config_set, ufsbsg, ufsframework, bsg)
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
