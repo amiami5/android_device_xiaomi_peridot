e280a0954fe66d4ddf6a84784082efb6f3d083c9
kenway214 <kenway214@outlook.com>
2025-06-27 02:06:05 +0530

    peridot: Build vendor variant of libinput_shim & libbinder_shim
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- device.mk
+++ device.mk
@@ -674,8 +674,8 @@ PRODUCT_PACKAGES += \
     android.media.audio.common.types-V3-cpp \
     libdisplayconfig.system.qti \
     libgui_shim \
-    libinput_shim \
-    libbinder_shim \
+    libinput_shim.vendor \
+    libbinder_shim.vendor \
     libwfdaac_vendor \
     vendor.display.config@2.0 \
     vendor.qti.hardware.display.config-V5-ndk
