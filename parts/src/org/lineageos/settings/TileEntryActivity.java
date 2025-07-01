5beb0e299a1f44b56e9db9d32d39d8d3823ae9c9
Guidix <guidixm@gmail.com>
2025-07-01 20:22:16 +0300

    peridot: parts: Add long press action for HTSR Tile
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- parts/src/org/lineageos/settings/TileEntryActivity.java
+++ parts/src/org/lineageos/settings/TileEntryActivity.java
@@ -30,6 +30,7 @@ import org.lineageos.settings.thermal.ThermalSettingsActivity;
 import org.lineageos.settings.saturation.SaturationActivity;
 import org.lineageos.settings.gamebar.GameBarSettingsActivity;
 import org.lineageos.settings.turbocharging.TurboChargingActivity;
+import org.lineageos.settings.touchsampling.TouchSamplingSettingsActivity;
 
 public class TileEntryActivity extends Activity {
     private static final String TAG = "TileEntryActivity";
@@ -37,6 +38,7 @@ public class TileEntryActivity extends Activity {
     private static final String SATURATION_TILE = "org.lineageos.settings.saturation.SaturationTileService";
     private static final String GAMEBAR_TILE = "org.lineageos.settings.gamebar.GameBarTileService";
     private static final String TURBOCHG_TILE = "org.lineageos.settings.turbocharging.TurboChargingTile";
+    private static final String HTSR_TILE = "org.lineageos.settings.touchsampling.TouchSamplingTileService";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,6 +61,8 @@ public class TileEntryActivity extends Activity {
             intent = new Intent(this, GameBarSettingsActivity.class);
         } else if (TURBOCHG_TILE.equals(sourceClassName)) {
             intent = new Intent(this, TurboChargingActivity.class);
+        } else if (HTSR_TILE.equals(sourceClassName)) {
+            intent = new Intent(this, TouchSamplingSettingsActivity.class);
         } else {
             Log.e(TAG, "Unknown tile: " + sourceClassName);
             finish();
