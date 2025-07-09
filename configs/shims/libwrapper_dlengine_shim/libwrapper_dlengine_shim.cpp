2aca61782a14591052b78dc75a6add72e904993a
Guidix <guidixm@gmail.com>
2025-07-09 17:41:59 +0300

    peridot: Shim some camera libs with libwrapper_dlengine_shim
    07-09 15:57:07.680  5179  5179 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1223 LibGetAddr() Error: dlsym(ChiNodeEntry): undefined symbol: ChiNodeEntry
    07-09 15:57:07.695  5179  5179 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.dlengine.so'
    07-09 15:57:07.713  5179  5179 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.mawsaliency.so'
    07-09 15:57:07.715  5179  5179 W linker  : Warning: "/odm/lib64/libmibokeh_845_video.so" unused DT entry: DT_RPATH (type 0xf arg 0x2868) (ignoring)
    07-09 15:57:07.716  5179  5179 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1147 LibMap() dlopen: dlopen failed: cannot locate symbol "_ZN3zdl8DlSystem11TensorShapeD1Ev" referenced by "/odm/lib64/libmibokeh_845_video.so"..., on '/odm/lib64/camera/components/com.mi.node.videobokeh.so'
    07-09 15:57:07.725  5179  5179 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:1223 LibGetAddr() Error: dlsym(ChiAWBAlgorithmEntry): undefined symbol: ChiAWBAlgorithmEntry
    07-09 15:57:07.725  5179  5179 E CamX    : [ERROR][CHI    ] camxchicomponent.cpp:317 GetAWBAlgorithmCallback() Failed to retrieve ChiAWBAlgorithmEntry function address. from lib:/odm/lib64/camera/components/com.qti.stats.awbr.so
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- configs/shims/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
+++ configs/shims/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
@@ -1,5 +1,10 @@
 #include <stddef.h>
+#include <string>
 
 extern "C" {
     void *_ZTVN3zdl8DlSystem21UserBufferEncodingTfNE = nullptr;
+    void* ChiNodeEntry = nullptr;
+    void* ChiAWBAlgorithmEntry = nullptr;
+    void _ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE(void*) {}
+    void _ZN3zdl8DlSystem11TensorShapeD1Ev(void*) {}
 }
