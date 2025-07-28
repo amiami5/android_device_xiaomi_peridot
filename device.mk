bf6cecd0f3363c8d2f931993c4c3751733ad143f
Guidix <guidixm@gmail.com>
2025-07-28 17:22:27 +0300

    Revert "peridot: Switch to AIDL thermal HAL"
    this reverts commit ad61659a690ac4348861376de29cb7ef514809be
    not working properly

--- device.mk
+++ device.mk
@@ -595,9 +595,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # Thermal
-$(call soong_config_set,qti_thermal,netlink,true)
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.qti \
     android.hardware.thermal-V1-ndk.vendor \
     android.hardware.thermal@2.0.vendor
 
