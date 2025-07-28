8def833daeec0aa246f5d5045f3cfa8d86d1346b
Guidix <guidixm@gmail.com>
2025-07-28 03:27:56 +0300

    peridot: Drop Lineage Health
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -366,14 +366,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     $(LOCAL_PATH)/configs/linker.config.json
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/night_charging)
-
 # Logging
 include $(LOCAL_PATH)/configs/logging.mk
 
