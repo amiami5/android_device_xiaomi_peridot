7afee4f6405fb2c4cd13a0ec2e94d323283ed616
Guidix <guidixm@gmail.com>
2025-08-21 01:49:41 +0300

    peridot: Drop Dolby Vision
    Dolby vision depends on dolby audio blobs, so nuke it
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -236,11 +236,6 @@ blob_fixups: blob_fixups_user_type = {
      ): blob_fixup()
         .add_needed('libcodec2_hidl_shim.so'),
 
-    ('vendor/etc/media_codecs.xml',
-     'vendor/etc/media_codecs_cliffs_v0.xml'
-    ): blob_fixup()
-       .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
-
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
 
@@ -253,7 +248,9 @@ blob_fixups: blob_fixups_user_type = {
 
      'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace('.+DOLBY.+\n', '')
-        .regex_replace('.+<!-- DOLBY.+\n', ''),
+        .regex_replace('.+<!-- DOLBY.+\n', '')
+        .regex_replace('.+<hal.*name=".*dv.*".*>\n', '')
+        .regex_replace('.+<hal.*name=".*dolby.*".*>\n', ''),
 
     'vendor/lib64/libqcrilNr.so|vendor/lib64/libril-db.so': blob_fixup()
         .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
@@ -280,6 +277,50 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/etc/media_codecs_performance_cliffs_v0.xml'): blob_fixup()
         .regex_replace(
             '.+media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).+\n',
+            ''
+        )
+        .regex_replace(
+            r'        <MediaCodec name="c2\.qti\.dv\.(decoder|encoder).*type="video/dolby-vision".*>\n(.*\n)*?        </MediaCodec>\n',
+            ''
+        )
+        .regex_replace(
+            r'        <MediaCodec name="c2\.qti\.dv\.decoder\.secure".*type="video/dolby-vision".*>\n(.*\n)*?        </MediaCodec>\n',
+            ''
+        ),
+
+    'vendor/etc/kvh2xml.xml': blob_fixup()
+        .regex_replace(
+            '.+<TAG id="0xc000033" name="dolby_effect_param_tag"/>.*\n',
+            ''
+        ),
+
+    'vendor/etc/clstc_config_library.xml': blob_fixup()
+        .regex_replace(
+            r'(<library>\s*<name>libdolbyclstc\.so</name>\s*<priority>1</priority>\s*)<enable>1</enable>',
+            r'\1<enable>0</enable>'
+        ),
+
+    'product/etc/device_features/peridot.xml': blob_fixup()
+        .regex_replace(
+            r'<bool name="support_dolby_version_brighten">true</bool>',
+            '<bool name="support_dolby_version_brighten">false</bool>'
+        )
+        .regex_replace(
+            r'<bool name="gallery_support_dolby">true</bool>',
+            '<bool name="gallery_support_dolby">false</bool>'
+        ),
+
+    'vendor/etc/media_cliffs_v0/video_system_specs.json': blob_fixup()
+        .regex_replace(
+            '        "DolbyVision": \{\s*"profiles": \[4, 5, 8\],\s*"max_main_tier_level": 12,\s*"max_high_tier_level": 12\s*\},',
+            ''
+        )
+        .regex_replace(
+            '.+//dolby post process.*\n',
+            ''
+        )
+        .regex_replace(
+            '.+"libqcodec2_dolbydecoderfilter\.so",.*\n',
             ''
         ),
 
