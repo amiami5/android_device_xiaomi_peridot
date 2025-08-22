c7af616b741419d3a686bf310034049adba55fc5
Guidix <guidixm@gmail.com>
2025-08-22 23:43:29 +0300

    peridot: Update libwrapper_dlengine_shim
    dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEFactory17getLibraryVersionEv" referenced by "/odm/lib64/libmibokeh_845_video.so"..., on '/odm/lib64/camera/components/com.mi.node.videobokeh.so'
    dlopen failed: cannot locate symbol "_ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE" referenced by "/odm/lib64/libwrapper_dlengine.so"..., on '/odm/lib64/camera/components/com.mi.node.mawsaliency.so'
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- configs/shims/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
+++ configs/shims/libwrapper_dlengine_shim/libwrapper_dlengine_shim.cpp
@@ -7,4 +7,8 @@ extern "C" {
     void* ChiAWBAlgorithmEntry = nullptr;
     void _ZN3zdl11DlContainer12IDlContainer4openERKNSt6__ndk112basic_stringIcNS2_11char_traitsIcEENS2_9allocatorIcEEEE(void*) {}
     void _ZN3zdl8DlSystem11TensorShapeD1Ev(void*) {}
+    void _ZN3zdl4SNPE11SNPEFactory17getLibraryVersionEv(void*) {}
+    void _ZN3zdl4SNPE11SNPEBuilderC1EPNS_11DlContainer12IDlContainerE(void*) {}
+    void _ZN3zdl4SNPE11SNPEFactory16getTensorFactoryEv(void*) {}
+    void _ZN3zdl4SNPE11SNPEBuilder19setRuntimeProcessorENS_8DlSystem9Runtime_tE(void*) {}
 }
