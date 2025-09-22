c3ee5661ca78d185123a6a3cb58932737dd8e86f
ami <github@amisuke.com>
2025-09-22 16:57:28 +0900

    vendorsetup.shの書き換え
    Change-Id: If03dea2af36b92009f5dc7594b28d3f4798dc6c2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,48 +8,15 @@
   git cherry-pick be29bcf0c9542a8417199528544f9fbea9c9e015
 )
 
-# Vendor (fresh clone)
-echo "Cloning vendor tree..."
-rm -rf vendor/xiaomi/peridot
-git clone -b 16 https://github.com/sm8635-dev/vendor_xiaomi_peridot.git vendor/xiaomi/peridot
-
-# Kernel (fresh clone)
-echo "Cloning kernel tree..."
-rm -rf device/xiaomi/peridot-kernel
-git clone -b 16 https://github.com/sm8635-dev/device_xiaomi_peridot-kernel.git device/xiaomi/peridot-kernel
 
 # Hardwaee Sony (fresh clone)
 echo "Cloning Sony hardware..."
 rm -rf hardware/sony/timekeep
 git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep
 
-# Hardware Xiaomi (fresh clone)
-echo "Cloning Xiaomi hardware..."
-rm -rf hardware/xiaomi
-git clone -b axion-16 https://github.com/sm8635-dev/hardware_xiaomi.git hardware/xiaomi
 
-# Dolby cloning
-echo "Cloning Dolby Atmos..."
-rm -rf hardware/dolby
-git clone -b 16-oss https://github.com/sm8635-dev/hardware_dolby.git hardware/dolby
 
-# MiuiCamera device tree (fresh clone)
-echo "Cloning MiuiCamera device tree..."
-rm -rf device/xiaomi/peridot-miuicamera
-git clone https://github.com/sm8635-dev/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
 
-# MiuiCamera vendor tree (fresh clone)
-echo "Cloning MiuiCamera vendor tree..."
-rm -rf vendor/xiaomi/peridot-miuicamera
-git clone https://github.com/sm8635-dev/vendor_xiaomi_peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
-
-# Refresh signing keys
-if [ -d vendor/lineage-priv/keys ]; then
-  echo "Removing existing signing keys..."
-  rm -rf vendor/lineage-priv/keys
-fi
-echo "Cloning fresh signing keys..."
-git clone https://github.com/Neon-Duchamp/keys.git -b old-keys vendor/lineage-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then
