c1e3bc933291cd941195b946ccd0acf614cba817
ami <github@amisuke.com>
2025-10-03 23:23:36 +0900

    venddorsetupを削除
    固定する必要なかったかもしれない
    
    Change-Id: I85f426526b94a1d789a76d63b9ab8991d31c1162

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
-# This script is sourced after the lunch command.
-
 #7ac03d638c07bac1cb1ae9749fdf7e0db60b4a7a 1.0.4
 #7a08683c741f9d42bea8cb7f28823f07a57332bc 1.0.5
 #1d258196b1abdeec0437c0c5f0c748e05ef61300 1.0.6
@@ -11,20 +9,22 @@
 # Assuming the submodule exists, forcefully check out a known stable commit.
 
 # 'T' variable is set by envsetup.sh to the top of the source tree.
-if [ -n "$T" ]; then
-    STABLE_KSU_COMMIT="9d9f9ed5d5f3a66a2f44e24c926075554f9275ce"
-    KSU_SUBMODULE_PATH="$T/kernel/xiaomi/sm8635/KernelSU-Next"
 
-    echo "==> Forcefully pinning KernelSU to stable commit..."
+#if [ -n "$T" ]; then
+#    STABLE_KSU_COMMIT="f40fe9b9e1e6ff09686dcc68af8a4cee52d48bf2"
+#    KSU_SUBMODULE_PATH="$T/kernel/xiaomi/sm8635/KernelSU-Next"
+
+#    echo "==> Forcefully pinning KernelSU to stable commit..."
 
     # Use a subshell to perform git operations.
     # The '|| true' ensures the script won't stop even if git commands fail.
-    (
-        cd "$KSU_SUBMODULE_PATH" && \
-        git fetch --all && \
-        git checkout "$STABLE_KSU_COMMIT"
-    ) || true
+#    (
+#        cd "$KSU_SUBMODULE_PATH" && \
+#        git fetch --all && \
+#        git checkout "$STABLE_KSU_COMMIT"
+#    ) || true
 
-    echo "KernelSU pinning process attempted."
-fi
+#    echo "KernelSU pinning process attempted."
+#fi
+
 # --- End of KernelSU Version Pinning ---
